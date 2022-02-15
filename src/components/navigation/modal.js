c319a39f4d43e8ec3e4c6b75f44b42a8fc578d76
Dwight Smith <dwightsmth@gmail.com>
2022-02-14 23:58:11 -0500

    add emailData functions for modal form

--- src/components/navigation/modal.js
+++ src/components/navigation/modal.js
@@ -1,22 +1,44 @@
+import { useState } from "react";
+
 const Modal = ({setIsOpen}) => {
+    const [emailData, setEmailData] = useState({name: '', email: '', message: ''});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //send email using provided emailData 
+        clear();
+    }
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        setEmailData({...emailData, [name]: value});
+    }
+
+    const clear = () => {
+        setEmailData({name: '', email: '', message: ''});
+    }
+    
     return (
         <>
             <div className="modal-overlay" onClick={() => setIsOpen(false)}></div>
             <div className="modal-content">
                 <section>
                     <div className='modal-title'>Contact Me!</div>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label>
                             <span>Name:</span>
-                            <input type='text' name='sender_name' />
+                            <input value={ emailData.name } onChange={ handleChange } type='text' name='name' />
                         </label>
                         <label>
                             <span>Email:</span>
-                            <input type='email' name='sender_email' />
+                            <input value={ emailData.email } onChange={ handleChange } type='email' name='email' />
                         </label>
                         <label>
                             <span>Message:</span>
-                            <textarea type='message' name='sender_message' />
+                            <textarea value={ emailData.message } onChange={ handleChange } type='message' name='message' />
                         </label>
                         <div className='submit-container'>
                             <input type='submit' value='Send' />
