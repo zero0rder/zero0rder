4cc1463f0fdf96ab045b84c47ecd11844a09d890
Dwight Smith <dwightsmth@gmail.com>
2022-06-14 22:10:40 -0400

    matchMedia support

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import NavigationBar from './components/navigation/navigationBar';
-import MenuPanel from './components/navigation/menuPanel';
+import Desktop from './components/desktop/desktop';
+import { MobileLanding } from './components/mobile/landing';
 import './App.css';
+//this.containerRef = React.createRef();
+// toggleMenu = () => {
+//     const contentContainer = this.containerRef.current;
+//     contentContainer.classList.toggle('is-nav-open');
+// }
+
+const viewportQuery = window.matchMedia('(max-width: 448px)');
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.containerRef = React.createRef();
-  } 
-
-  toggleMenu = () => {
-      const contentContainer = this.containerRef.current;
-      contentContainer.classList.toggle('is-nav-open');
+    this.state = {
+      aboutModalOpen: false,
+      projectsModalOpen: false,
+      isMobile: viewportQuery.matches
+    };
+    
   }
-  
-  render(){
+
+  componentDidMount(){
+    viewportQuery.onchange = () => this.setState(state => ({...state, isMobile: viewportQuery.matches}))
+  }
+
+  componentWillUnmount(){
+    viewportQuery.removeEventListener(viewportQuery.onchange)
+  }
+
+  // componentDidUpdate(){}
+
+  modalToggle = (modalType) => this.setState(state => ({...state, [modalType]: !state[modalType]}))
+
+  render() {
     return (
-        <div ref={this.containerRef} className='app'>
-          <MenuPanel />
-          <section className='content-section'>
-            <NavigationBar toggleMenu={this.toggleMenu}/>
-            <Outlet />
-          </section>
-        </div>
+      <div className='app'>
+        <section className='content-section'>
+          {
+            this.state.isMobile ? (<MobileLanding/>) : (
+                <>
+                  <NavigationBar toggle={this.modalToggle} />
+                  <Desktop toggle={this.modalToggle} flags={[this.state.aboutModalOpen, this.state.projectsModalOpen]} />
+                </>
+            )
+          }
+          {/* <Outlet /> */}
+        </section>
+      </div>
     )
   }
 }
