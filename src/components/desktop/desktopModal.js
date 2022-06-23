acc29bae6696ee08a5a5b296a073a474ef5c3e65
Dwight Smith <dwightsmth@gmail.com>
2022-06-23 13:35:32 -0400

    added draggable support for desktop modals

--- src/components/desktop/desktopModal.js
+++ src/components/desktop/desktopModal.js
@@ -1,5 +1,6 @@
-// import { useState } from "react";
+import { useRef } from "react";
 import { DesktopModal, DesktopModalHeader, DesktopModalCloseButton } from "./styled";
+import Draggable from 'react-draggable';
 
 const setZIndex = (target) => {
     Array.from(target.closest('.dsktp-container').childNodes).map((e, i) => {
@@ -14,14 +15,20 @@ const setZIndex = (target) => {
 
 export const DesktopModalPopup = (props) => {
     const closeModal = () => props.title === 'About' ? props.toggle('aboutModalOpen') : props.toggle('projectsModalOpen');
-    
+    const modalRef = useRef();
+
     return (
-        <DesktopModal className={props.className} onClick={(e) => setZIndex(e.currentTarget)}>
-            <DesktopModalHeader>{props.title}</DesktopModalHeader>
-            <DesktopModalCloseButton onClick={() => closeModal()}>
-                <span className="iconify" data-icon="icon-park:close" data-width="256" data-height="256"></span>
-            </DesktopModalCloseButton>
-            {props.children}
-        </DesktopModal>
+        <Draggable
+            handle='.modal-header'
+            nodeRef={modalRef}
+            bounds='body'>
+            <DesktopModal ref={modalRef} className={props.className} onClick={(e) => setZIndex(e.currentTarget)}>
+                <DesktopModalHeader className='modal-header'>{props.title}</DesktopModalHeader>
+                <DesktopModalCloseButton onClick={() => closeModal()}>
+                    <span className="iconify" data-icon="icon-park:close" data-width="256" data-height="256"></span>
+                </DesktopModalCloseButton>
+                {props.children}
+            </DesktopModal>
+        </Draggable>
     )
 }
