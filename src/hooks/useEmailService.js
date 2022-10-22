c7a6fc8507e08f2b861be13983853c9d3837bf81
Dwight Smith <dwightsmth@gmail.com>
2022-10-21 20:22:04 -0400

    icon script add + email hook updates

--- src/hooks/useEmailService.js
+++ src/hooks/useEmailService.js
@@ -1,39 +1,33 @@
-import { useState } from "react";
-import{ init, send } from '@emailjs/browser';
-import { emailVARS } from '../utils/email/emailData';
-init(emailVARS.UserID);
+import { useState } from "react"
+import{ init, send } from '@emailjs/browser'
+import { emailVARS } from '../utils/email/emailData'
+init(emailVARS.UserID)
 
 const useEmailService = (initialState) => {
-    const [emailData, setEmailData] = useState(initialState);
+    const [emailData, setEmailData] = useState(initialState)
+    const [status, setStatus] = useState(null)
 
     const handleChange = (e) => {
-        e.preventDefault();
-        const { name, value } = e.target;
-        setEmailData({...emailData, [name]: value});
+        e.preventDefault()
+        const { name, value } = e.target
+        setEmailData({...emailData, [name]: value})
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         send(emailVARS.ServiceID, emailVARS.TemplateID, {
             from_name: emailData.name,
             to_name: emailData.ToName,
             message: emailData.message,
             reply_to: emailData.email,
         }, emailVARS.UserID).then((result) => {
-            console.log('success', result.text);
+            setStatus(() => result.status)
         }, (error) => {
-            console.log('oops, something went wrong', error.text);
-        });
-
-        clear();
+            console.log('oops, something went wrong', error.text)
+        })
     }
 
-    const clear = () => {
-        setEmailData(initialState);
-    }
-
-    return { handleChange, handleSubmit, emailData }
-
+    return { handleChange, handleSubmit, emailData, status }
 }
 
-export default useEmailService;
+export default useEmailService
