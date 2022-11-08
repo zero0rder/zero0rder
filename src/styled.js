a005fc764ec0373a67d3e71acddb933e64e6766b
Dwight Smith <dwightsmth@gmail.com>
2022-11-07 19:54:17 -0500

    base styles

--- src/styled.js
+++ src/styled.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
+import { Row } from 'antd'
 
-export const AppContainer = styled.div``;
-
-export const ContentSection = styled.section`
-    height: 100vh;
-    background-image: linear-gradient(to ${ props => props.mobile ? 'right' : 'left' } top, #dadad3, #b2bcb0, #84a097, #578487, #32667a, #25546f, #1e4262, #1c3053, #142849, #0c203e, #051835, #000f2b);
+export const AppContainer = styled(Row)`
+    height: inherit;
+    padding-bottom: 125px;
 `;
