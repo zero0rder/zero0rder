534c8e0f82296e7a0916362d934c5a4c9ce010bb
Dwight Smith <dwightsmth@gmail.com>
2023-01-31 12:08:34 -0500

    fix: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,53 @@
-import { useState, useRef, useEffect } from 'react'
-import { motion, useScroll, useSpring } from 'framer-motion'
-import Navbar from './components/navbar'
-import Hero from './components/hero'
-import OpenSource from './components/opensource'
-import Contact from './components/contact'
-import Footer from './components/footer'
-import Resume from './components/resume'
-import { ResumeContext, ContactWrapperContext, OSWrapperContext } from './contexts'
-import { AppContainer, ContentWrap } from './components/styled/app'
-import ScrollTopBtn from './components/scrollTopBtn'
+import { useState, useRef, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+// import { motion, useScroll, useSpring } from "framer-motion";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Resume from "./components/resume";
+import {
+  ResumeContext,
+  ContactWrapperContext,
+  OSWrapperContext,
+} from "./contexts";
+import { AppContainer, ContentWrap } from "./components/styled/app";
 
 function App() {
-  const [isResume, setIsResume] = useState<boolean>(false)
-  const [osElem, setOSElem] = useState<HTMLDivElement | null>(null)
-  const [contactElem, setContactElem] = useState<HTMLDivElement | null>(null)
-  const { scrollYProgress } = useScroll()
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 300,
-    damping: 30,
-    restDelta: 0.001
-  })
-  
-  const osRef = useRef<HTMLDivElement | null>(null)
-  const contactRef = useRef<HTMLDivElement | null>(null)
+  const [isResume, setIsResume] = useState<boolean>(false);
+  const [osElem, setOSElem] = useState<HTMLDivElement | null>(null);
+  const [contactElem, setContactElem] = useState<HTMLDivElement | null>(null);
+  // const { scrollYProgress } = useScroll();
+  // const scaleX = useSpring(scrollYProgress, {
+  //   stiffness: 300,
+  //   damping: 30,
+  //   restDelta: 0.001,
+  // });
+
+  const osRef = useRef<HTMLDivElement | null>(null);
+  const contactRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if(osRef.current)
-      setOSElem(() => osRef.current)
+    if (osRef.current) setOSElem(() => osRef.current);
 
-    if(contactRef.current)
-      setContactElem(() => contactRef.current)
-
-  },[osRef.current, contactRef.current])
+    if (contactRef.current) setContactElem(() => contactRef.current);
+  }, [osRef.current, contactRef.current]);
 
   return (
-    <AppContainer className='app'>
-      <motion.div className='progress-bar' style={{ scaleX }}/>
-      <ResumeContext.Provider value={{isResume, setIsResume}}>
-        <ContactWrapperContext.Provider value={{element: contactElem}}>
-          <OSWrapperContext.Provider value={{element: osElem}}>
-            <Navbar/>
-            <ContentWrap>
-              <Hero/>
-              <OpenSource ref={osRef}/>
-              <Contact ref={contactRef}/>
+    <AppContainer className="app">
+      {/* <motion.div className="progress-bar" style={{ scaleX }} /> */}
+      <ResumeContext.Provider value={{ isResume, setIsResume }}>
+        <ContactWrapperContext.Provider value={{ element: contactElem }}>
+          <OSWrapperContext.Provider value={{ element: osElem }}>
+            <Navbar />
+            <ContentWrap style={{ height: `${window.innerHeight - 104}px` }}>
+              <Outlet />
             </ContentWrap>
-            <Footer/>
-            <Resume/>
+            <Footer />
+            <Resume />
           </OSWrapperContext.Provider>
         </ContactWrapperContext.Provider>
       </ResumeContext.Provider>
-      <ScrollTopBtn/>
     </AppContainer>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GlobalStyles from './styles/globalStyles'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./styles/globalStyles";
+import App from "./App";
+import Hero from "./components/hero";
+import OpenSource from "./components/opensource";
+import Contact from "./components/contact";
+import About from "./components/about";
+import Blog from "./components/blog";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyles/>
-    <App />
+    <GlobalStyles />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Hero />} />
+          <Route path="/opensource" element={<OpenSource />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
